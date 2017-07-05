bb21813794beef70d9b81e2393e462d067a55345
clementine <clementine.thornary@gmail.com>
2017-07-05 11:23:09 +0200

    after fixing code

--- src/Lib/StringFormatter.php
+++ src/Lib/StringFormatter.php
@@ -2,44 +2,44 @@
 
 namespace Lib;
 
-class StringFormatter {
+class StringFormatter
+{
+    public static function concat($str1, $str2)
+    {
+        if (!is_string($str1) || !is_string($str2)) {
+            return false;
+        }
+        return $str1 . $str2;
+    }
 
+    public static function toCamelCase($str1, $str2)
+    {
+        if (!is_string($str1) || !is_string($str2)) {
+            return false;
+        }
+        return ucfirst(strtolower($str1)).ucfirst(strtolower($str2));
+    }
 
-	public static function concat($str1, $str2) {
-		if (!is_string($str1) || !is_string($str2)) {
-			return false;
-		}
-		return $str1 . $str2;
-	}
+    
+    public static function prefix($str1, $str2, $toCamelCase = false)
+    {
+        if (!is_string($str1) || !is_string($str2)) {
+            return false;
+        }
+        if ($toCamelCase) {
+            return self::toCamelCase($str1, $str2);
+        }
+        return self::concat($str1, $str2);
+    }
 
-	public static function toCamelCase($str1, $str2) {
-		if (!is_string($str1) || !is_string($str2)) {
-			return false;
-		} 
-		return ucfirst(strtolower($str1)).ucfirst(strtolower($str2));
-	}
-
-	
-	public static function prefix($str1, $str2, $toCamelCase = false) {
-		if (!is_string($str1) || !is_string($str2)) {
-			return false;
-		} 
-		if ($toCamelCase) {
-			return self::toCamelCase($str1, $str2);
-		}
-		return self::concat($str1, $str2);
-	}
-
-	public static function suffix($str1, $str2, $toCamelCase = false) {
-		if (!is_string($str1) || !is_string($str2)) {
-					return false;
-		} 
-		if ($toCamelCase) {
-			return self::toCamelCase($str2, $str1);
-		}
-		return self::concat($str2, $str1);
-	}
-
-} 
-
-?>
+    public static function suffix($str1, $str2, $toCamelCase = false)
+    {
+        if (!is_string($str1) || !is_string($str2)) {
+            return false;
+        }
+        if ($toCamelCase) {
+            return self::toCamelCase($str2, $str1);
+        }
+        return self::concat($str2, $str1);
+    }
+}
--- tests/Lib/StringFormatterTest.php
+++ tests/Lib/StringFormatterTest.php
@@ -3,45 +3,50 @@
 use Lib\StringFormatter;
 use PHPUnit\Framework\TestCase;
 
-class StringFormatterTest extends TestCase {
+class StringFormatterTest extends TestCase
+{
+    private $str1;
+    private $str2;
 
-	private $str1;
-	private $str2;
+    public function setUp()
+    {
+        $this->str1 = "hello";
+        $this->str2 = "world";
+    }
 
-	public function setUp() {
-		$this->str1 = "hello";
-		$this->str2 = "world";
-	}
+    public function testConcat()
+    {
+        $actual = StringFormatter::concat($this->str1, $this->str2);
+        $this->assertSame('helloworld', $actual);
+    }
 
-	public function testConcat() {
-		$actual = StringFormatter::concat($this->str1, $this->str2);
-		$this->assertSame('helloworld', $actual);
-	}
+    public function testToCamelCase()
+    {
+        $actual = StringFormatter::toCamelCase($this->str1, $this->str2);
+        $this->assertSame('HelloWorld', $actual);
+    }
 
-	public function testToCamelCase() {
-		$actual = StringFormatter::toCamelCase($this->str1, $this->str2);
-		$this->assertSame('HelloWorld', $actual);
-	}
+    public function testPrefix()
+    {
+        $actual = StringFormatter::prefix($this->str1, $this->str2);
+        $this->assertSame('helloworld', $actual);
+    }
 
-	public function testPrefix() {
-		$actual = StringFormatter::prefix($this->str1, $this->str2);
-		$this->assertSame('helloworld', $actual);
-	}
+    public function testPrefixWithCamelCase()
+    {
+        $actual = StringFormatter::prefix($this->str1, $this->str2, true);
+        $this->assertSame('HelloWorld', $actual);
+    }
 
-	public function testPrefixWithCamelCase() {
-		$actual = StringFormatter::prefix($this->str1, $this->str2, true);
-		$this->assertSame('HelloWorld', $actual);
-	}
+    public function testSuffix()
+    {
+        $actual = StringFormatter::suffix($this->str1, $this->str2);
+        $this->assertSame('worldhello', $actual);
+    }
 
-	public function testSuffix() {
-		$actual = StringFormatter::suffix($this->str1, $this->str2);
-		$this->assertSame('worldhello', $actual);
-	}
-
-	public function testSuffixWithCamelCase() {
-		$actual = StringFormatter::suffix($this->str1, $this->str2, true);
-		$this->assertSame('WorldHello', $actual);
-	}
+    public function testSuffixWithCamelCase()
+    {
+        $actual = StringFormatter::suffix($this->str1, $this->str2, true);
+        $this->assertSame('WorldHello', $actual);
+    }
 }
-
-?>
